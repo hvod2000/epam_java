8a3a97fd19ac288cfb7c67de65357c0d6bb42ca6
Uladzislau Khamkou <kham@tuta.io>
2022-05-09 16:19:40 +0300

    remove code duplication in final#3

--- final3/src/Runner3.java
+++ final3/src/Runner3.java
@@ -11,42 +11,42 @@ public class Runner3 {
             // 1. Создать одиночные объекты всех классов, умеющих издавать звуки,
             // и записать все изданные ими звуки в заданный файл.
             Dog dog = new Dog();
-            Cat cat = new Cat();
             Car car = new Car();
+            Cat cat = new Cat();
             fileWriter.write("TASK #1\n");
-            fileWriter.write(dog.makeSound() + "; ");
             fileWriter.write(cat.makeSound() + "; ");
             fileWriter.write(car.makeSound() + ";\n");
+            fileWriter.write(dog.makeSound() + "; ");
 
             // 2. Создать массив объектов класса Car и
             // записать все изданные ими звуки в тот же файл.
             Car[] cars = {new Car(), new Car(), new Car()};
             fileWriter.write("TASK #2\n");
-            for (Car source: cars) {
-                fileWriter.write("A " + source + " tells " + source.makeSound() + "\n");
-            }
+            speakToFile(cars, fileWriter);
 
             // 3. Создать массив объектов классов Dog и Cat
             // (в массиве одновременно должны находиться объекты обоих классов)
             // и записать все изданные ими звуки в тот же файл.
             Animal[] animals = {new Cat(), new Dog(), new Cat()};
             fileWriter.write("TASK #3\n");
-            for (Animal source: animals) {
-                fileWriter.write("A " + source + " tells " + source.makeSound() + "\n");
-            }
+            speakToFile(animals, fileWriter);
 
             // 4. Создать массив объектов классов Dog, Cat и Car
             // (в массиве одновременно должны находиться объекты всех трёх классов)
             // и записать все изданные ими звуки в тот же файл.
             IMakingSounds[] sources = {new Dog(), new Cat(), new Car()};
             fileWriter.write("TASK #4\n");
-            for (IMakingSounds source: sources) {
-                fileWriter.write("A " + source + " tells " + source.makeSound() + "\n");
-            }
+            speakToFile(sources, fileWriter);
 
             fileWriter.close();
         } catch (IOException exception) {
             System.err.println("ERROR! Failed to write to file!");
         };
+    }
+
+    private static void speakToFile(IMakingSounds[] sources, FileWriter file) throws IOException {
+        for (IMakingSounds source: sources) {
+            file.write("A " + source + " tells " + source.makeSound() + "\n");
+        }
     }
 }
